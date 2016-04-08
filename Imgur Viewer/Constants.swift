d890b5189239fb335f0963c8bb0f31af7437c6f8
jovito-royeca <jovit.royeca@gmail.com>
2016-04-08 10:36:36 +0800

    feat: Created Xcode project.

---
+++ Imgur Viewer/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Imgur Viewer
+//
+//  Created by Jovit Royeca on 08/04/2016.
+//  Copyright © 2016 Jovito Royeca. All rights reserved.
+//
+
+import Foundation
+
+struct Imgur {
+    static let ClientID = "8201c8834dbeb80"
+}
