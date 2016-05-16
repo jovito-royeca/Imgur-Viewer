46079a3b372751b290a07695f2e4eb201b2948e2
jovito-royeca <jovit.royeca@gmail.com>
2016-05-16 10:29:12 +0800

    feat: Added Cocoapod dependencies.

---
+++ Pods/Target Support Files/Pods-Imgursonate/Pods-Imgursonate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Imgursonate : NSObject
+@end
+@implementation PodsDummy_Pods_Imgursonate
+@end
---
+++ Pods/Target Support Files/Pods-ImgursonateUITests/Pods-ImgursonateUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ImgursonateUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ImgursonateUITestsVersionString[];
+
